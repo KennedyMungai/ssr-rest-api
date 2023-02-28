cedbea83012fae41aa1aeec002f8daaaa744ab84
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-28 20:21:25 +0300

    Wrote teh basic setup code for the component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,29 @@ import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import { useEffect } from 'react'
 
-export default function Home()
+export async function getServerSideProps()
+{
+  // The server side code
+}
+
+export default function Home({ users })
 {
   return (
-    
+    <ul>
+      {
+        users.map((user) =>
+          <li key={user.id}>
+            <Link
+              href={`/users/${user.username}`}
+              passHref
+            >
+              <a>
+                {user.username}
+              </a>
+            </Link>
+          </li>
+        )
+      }
+    </ul>
   )
 }
